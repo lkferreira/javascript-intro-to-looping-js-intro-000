004294c438f598a31784d1af2aa8990d2b4c79ed
Lucas Klefasz Ferreira <ferreirak.lucas@gmail.com>
2019-05-06 01:55:07 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,22 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+  if (i == 1) {
+    array = [...array, 'I am ' + i + ' strange loop.']
+  } else {
+    array = [...array, 'I am ' + i + ' strange loops.']
+  }
+}
+return array
+}
+
+function whileLoop (number) {
+  while (number > 0) {
+    console.log(--number)
+  }
+  return 'done'
+}
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
